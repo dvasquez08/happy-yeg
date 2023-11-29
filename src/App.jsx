c402c7306f099a80ba89f44b9ee24a66798bad08
dvasquez08 <dvasquez.422@gmail.com>
2023-11-29 05:24:03 -0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ButtonGroup from "./Components/ButtonGroup";
+
+function App() {
+  return (
+    <>
+      <div className="text-4xl flex items-center justify-center">
+        <h1>Happy YEG</h1>
+      </div>
+      <div>
+        <p>
+          These days, time are tough. It's getting harder to spend money when
+          you go out especially with prices going higher, but it doesn't mean
+          you can't go out and celebrate without breaking the bank! Take
+          advantage of happy hours to save you money while still being able to
+          go out.
+        </p>
+        <p>
+          Don't know what the happy hour specials are in the restaurants in your
+          area? Don't sweat, that's what this site is for! Here's a list of
+          happy hours throughout the city of Edmontont. This list will grow as
+          more research is done and will update as things change. Have a look,
+          and I hope you find a great place in your area!
+        </p>
+      </div>
+      <ButtonGroup />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/ButtonGroup.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function ButtonGroup() {
+  return (
+    <>
+      <h1 className="text-3xl flex items-center justify-center">
+        Select Location
+      </h1>
+      <div className="flex justify-evenly">
+        <button className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+          North
+        </button>
+        <button className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+          West
+        </button>
+        <button className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+          South
+        </button>
+        <button className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+          Downtown
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default ButtonGroup;
