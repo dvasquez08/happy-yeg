ff0449c1f84dc552ab9c416ac2af81aa749d4293
dvasquez08 <dvasquez.422@gmail.com>
2023-12-03 11:44:31 -0700

    Added functionality for mobile version menu

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,6 +4,7 @@ import ContactForm from "./ContactForm";
 
 function NavBar({ searchTerm, setSearchTerm }) {
   const [isContactFormOpen, setContactFormOpen] = useState(false);
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const openContactForm = () => {
     setContactFormOpen(true);
@@ -27,30 +28,6 @@ function NavBar({ searchTerm, setSearchTerm }) {
             </span>
           </a>
           <div className="flex md:order-2">
-            <button
-              type="button"
-              data-collapse-toggle="navbar-search"
-              aria-controls="navbar-search"
-              aria-expanded="false"
-              className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1"
-            >
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                />
-              </svg>
-              <span className="sr-only">Search</span>
-            </button>
             <div className="relative hidden md:block">
               <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                 <svg
@@ -83,11 +60,12 @@ function NavBar({ searchTerm, setSearchTerm }) {
               />
             </div>
             <button
-              data-collapse-toggle="navbar-search"
+              data-collapse-toggle="navbar-menu"
               type="button"
               className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="navbar-search"
-              aria-expanded="false"
+              aria-controls="navbar-menu"
+              aria-expanded={isMobileMenuOpen}
+              onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
             >
               <span className="sr-only">Open main menu</span>
               <svg
@@ -108,7 +86,9 @@ function NavBar({ searchTerm, setSearchTerm }) {
             </button>
           </div>
           <div
-            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
+              isMobileMenuOpen ? "flex" : "hidden"
+            }`}
             id="navbar-search"
           >
             <div className="relative mt-3 md:hidden">
